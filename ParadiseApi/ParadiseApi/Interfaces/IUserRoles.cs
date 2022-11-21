0f593872d4135676154e1923581d2e62365906ed
JustKaneri <justkaneri@yandex.ru>
2022-11-21 22:06:19 +0300

    pattern repository

---
+++ ParadiseApi/ParadiseApi/Interfaces/IUserRoles.cs
@@ -0,0 +1,9 @@
+﻿using ParadiseApi.Models;
+
+namespace ParadiseApi.Interfaces
+{
+    public interface IUserRoles
+    {
+        public ICollection<UserRole> GetUserRoles();
+    }
+}
---
+++ ParadiseApi/ParadiseApi/Repository/UserRoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using ParadiseApi.Data;
+using ParadiseApi.Interfaces;
+using ParadiseApi.Models;
+
+namespace ParadiseApi.Repository
+{
+    public class UserRoleRepository : IUserRoles
+    {
+        private DataContext _context;
+
+        public UserRoleRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<UserRole> GetUserRoles()
+        {
+            return _context.UserRoles.OrderBy(p=>p.Id).ToList();
+        }
+    }
+}
