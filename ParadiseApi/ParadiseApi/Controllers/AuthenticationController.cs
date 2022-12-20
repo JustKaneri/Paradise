1207efc22a663dea396fb67045069fa3ed1829e1
JustKaneri <justkaneri@yandex.ru>
2022-12-20 14:37:42 +0300

    generate token

--- ParadiseApi/ParadiseApi/Controllers/AuthenticationController.cs
+++ ParadiseApi/ParadiseApi/Controllers/AuthenticationController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ParadiseApi.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using ParadiseApi.Models;
 
 namespace ParadiseApi.Controllers
 {
@@ -16,5 +22,34 @@ namespace ParadiseApi.Controllers
             _userManager = userManager;
             _jwtConfig = jwtConfig;
         }
+
+        private string GenerateJwtToken(Users user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.UTF8.GetBytes(_jwtConfig.Secret);
+
+            //Token descriptor
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.Id.ToString()),
+                    new Claim("role", user.Role.Name),
+                    new Claim(JwtRegisteredClaimNames.Name, user.Name),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString())
+                }),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+
+            return jwtToken;
+        }
+
     }
 }
