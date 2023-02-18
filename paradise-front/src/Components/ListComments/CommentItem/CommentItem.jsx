976d51e24484675307dc3ce2d38c17a2cf5bc4ca
JustKaneri <justkaneri@yandex.ru>
2023-02-18 18:33:53 +0300

    router to profile from comments

--- paradise-front/src/Components/ListComments/CommentItem/CommentItem.jsx
+++ paradise-front/src/Components/ListComments/CommentItem/CommentItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './commentIte.module.css'
 import getSrcUser from '../../../UserHook/useSrcUser';
+import { useNavigate } from 'react-router-dom';
 
 const CommentItem = ({comment}) => {
+
+    const router = useNavigate();
+
     return (
         <div className={styles.box}>
             <img
+                onClick={()=> router(`/profile/${comment.user.id}`)}
                 className={styles.avatar} 
                 src = {getSrcUser.Avatar(comment.user)}
             />
